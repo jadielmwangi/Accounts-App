0e884d52af65b7a42aca988fa5a3c57d0c9864c4
jadielmwangi <jedielmwangi@gmail.com>
2020-04-19 21:54:25 +0300

    adds Set up method to run before each test cases         test_init to test if the object is initialized properly

--- test.py
+++ test.py
@@ -29,10 +29,10 @@ class TestUser(unittest.TestCase):
 
     def test_save_user(self):
         '''
-        To test if the user object is saved into the user list
+        To test if the user object is saved into the user list,
         '''
 
-        self.new_user.save_user() #saving the new user
+        self.new_user.save_user()  #saving the new  user
         self.assertEqual(len(User.user_list),1)
 
     def tearDown(self):
